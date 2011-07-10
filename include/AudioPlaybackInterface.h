27927ca9e651aea9be8446a5d10af8559a51fb81
Chris Thomas <chrtho@umd.umich.edu>
2011-07-09 21:42:31 -0400

    Abstract class, AudioPlaybackInterface, to define the way playback will take place

---
+++ include/AudioPlaybackInterface.h
@@ -0,0 +1,29 @@
+#ifndef _AUDIO_PLAYBACK_INTERFACE_H_
+#define _AUDIO_PLAYBACK_INTERFACE_H_
+
+#include <string>
+#include "AudioSample.h"
+
+/**
+ * @brief Class that defines the interfaces necessary for audio playback functionality
+ */
+class AudioPlaybackInterface
+{
+public:
+	virtual ~AudioPlaybackInterface() {};
+
+	/** @brief Interface that will play back the audio data in \asamples */
+	virtual void playAudio(const AudioSample_t *samples, const size_t num_samples) = 0;
+	virtual void setVolume(const size_t new_volume) = 0;
+	virtual size_t getVolume() const = 0;
+	virtual void setRecordingOutputPath(const std::string & path) = 0;
+	virtual const std::string getRecordingOutputPath() const = 0;
+	virtual void startRecording() = 0;
+	virtual void stopRecording() = 0;
+	virtual void pauseRecording() = 0;
+	virtual bool currentlyRecording() const = 0;
+	virtual size_t getNumSamplesPlayed() const = 0;
+	virtual void resetNumSamplesPlayed() = 0;
+};
+
+#endif
---
+++ include/AudioSample.h
@@ -0,0 +1,8 @@
+#ifndef _AUDIO_SAMPLE_H_
+#define _AUDIO_SAMPLE_H_
+
+#include <stdint.h>
+
+typedef int16_t AudioSample_t;
+
+#endif
