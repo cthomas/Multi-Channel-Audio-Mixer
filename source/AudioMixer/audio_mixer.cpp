6d4e5aba4d92e83d7f77eb447c1cb17786ecb791
Chris Thomas <chrtho@umd.umich.edu>
2011-07-14 23:10:39 -0400

    Stubbing in setting the mixer as the playback channel on the PlaybackThread

--- source/AudioMixer/audio_mixer.cpp
+++ source/AudioMixer/audio_mixer.cpp
@@ -1,15 +1,21 @@
 
 #include <unistd.h>
 
+//TODO Replace this with the actual mixer
+#include "AudioMixerInterface.h"
 #include "DebugMessage.h"
 #include "PlaybackThread.h"
 
 int main(void)
 {
 	PlaybackThread *playback = PlaybackThread::startPlaybackThread();
+	AudioMixerInterface *mixer = NULL;
 
 	if(playback)
 	{
+		if(mixer)
+			playback->setPlaybackChannel(mixer);
+
 		size_t i = 0;
 
 		while(i++ < 5)
