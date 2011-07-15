6431a1ba0d05106f008248d2bfcb48887fa8c9ba
Chris Thomas <chrtho@umd.umich.edu>
2011-07-15 09:00:05 -0400

    Modifying const-ness of pop_front interface

--- include/AudioChannelInterface.h
+++ include/AudioChannelInterface.h
@@ -11,7 +11,7 @@ public:
 	virtual void push_back(const AudioSample_t & sample) = 0;
 	virtual void push_back(const std::vector<AudioSample_t> samples) = 0;
 	virtual void push_back(const AudioSample_t *samples, size_t num_samples) = 0;
-	virtual AudioSample_t pop_front() = 0;
+	virtual const AudioSample_t pop_front() = 0;
 	virtual const std::vector<AudioSample_t> pop_all() = 0;
 	virtual size_t size() = 0;
 };
--- include/AudioMixerInterface.h
+++ include/AudioMixerInterface.h
@@ -12,6 +12,14 @@ public:
 	virtual void addChannel(AudioChannelInterface *channel) = 0;
 	virtual void removeChannel(AudioChannelInterface *channel) = 0;
 	virtual size_t numChannels() const = 0;
+public: //AudioChannelInterface
+	virtual void push_back(const AudioSample_t & sample) = 0;
+	virtual void push_back(const std::vector<AudioSample_t> samples) = 0;
+	virtual void push_back(const AudioSample_t *samples, size_t num_samples) = 0;
+	virtual const AudioSample_t pop_front() = 0;
+	virtual const std::vector<AudioSample_t> pop_all() = 0;
+	virtual size_t size() = 0;
+
 };
 
 #endif
