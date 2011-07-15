26c0684c746e81ee68fcdf94f6b2cf9694d5e533
Chris Thomas <chrtho@umd.umich.edu>
2011-07-14 22:56:21 -0400

    Fixup foobar'd abstract classes

--- include/AudioChannelInterface.h
+++ include/AudioChannelInterface.h
@@ -1,17 +1,19 @@
 #ifndef _AUDIO_CHANNEL_INTERFACE_H_
 #define _AUDIO_CHANNEL_INTERFACE_H_
 
+#include <vector>
+
 #include "AudioSample.h"
 
 class AudioChannelInterface
 {
 public:
-	void push_back(const AudioSample_t & sample) = 0;
-	void push_back(const std::vector<AudioSample_t> samples) = 0;
-	void push_back(const AudioSample_t *samples, size_t num_samples) = 0;
-	AudioSample_t pop_front() = 0;
-	const std::vector<AudioSample_t> pop_all() = 0;
-	size_t size() = 0;
+	virtual void push_back(const AudioSample_t & sample) = 0;
+	virtual void push_back(const std::vector<AudioSample_t> samples) = 0;
+	virtual void push_back(const AudioSample_t *samples, size_t num_samples) = 0;
+	virtual AudioSample_t pop_front() = 0;
+	virtual const std::vector<AudioSample_t> pop_all() = 0;
+	virtual size_t size() = 0;
 };
 
 #endif
--- include/PlaybackInterface.h
+++ include/PlaybackInterface.h
@@ -1,17 +1,18 @@
 #ifndef _PLAYBACK_INTERFACE_H_
 #define _PLAYBACK_INTERFACE_H_
 
-#include "AudioChannel.h"
+#include "AudioChannelInterface.h"
 
 class PlaybackInterface
 {
+public:
 	typedef enum _PLAYBACK_INTERFACE_STATUS
 	{
 		IDLE,
 		PLAYING
 	}PLAYBACK_INTERFACE_STATUS;
-public:
-	void setPlaybackChannel(AudioChannel *channel) = 0;
-	const PLAYBACK_INTERFACE_STATUS getStatus() const = 0;
+
+	virtual void setPlaybackChannel(AudioChannelInterface *channel) = 0;
+	virtual const PLAYBACK_INTERFACE_STATUS getStatus() = 0;
 };
 #endif
