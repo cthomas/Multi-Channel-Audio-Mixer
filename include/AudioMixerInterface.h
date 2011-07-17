8e069fb36fd2fca1aba4d1e8eb78672569eb352d
Chris Thomas <chrtho@umd.umich.edu>
2011-07-16 21:12:21 -0400

    Removed extra funcs from AudioMixerInterface

--- include/AudioMixerInterface.h
+++ include/AudioMixerInterface.h
@@ -3,23 +3,12 @@
 
 #include <string.h>
 
-#include "AudioChannelInterface.h"
-
 class AudioMixerInterface
-	: public virtual AudioChannelInterface
 {
 public:
 	virtual void addChannel(AudioChannelInterface *channel) = 0;
 	virtual void removeChannel(AudioChannelInterface *channel) = 0;
 	virtual size_t numChannels() const = 0;
-public: //AudioChannelInterface
-	virtual void push_back(const AudioSample_t & sample) = 0;
-	virtual void push_back(const std::vector<AudioSample_t> samples) = 0;
-	virtual void push_back(const AudioSample_t *samples, size_t num_samples) = 0;
-	virtual const AudioSample_t pop_front() = 0;
-	virtual const std::vector<AudioSample_t> pop_all() = 0;
-	virtual size_t size() = 0;
-
 };
 
 #endif
