475d00193c59766341a8e8049fff3bf4ff8a53ac
Chris Thomas <chrtho@umd.umich.edu>
2011-07-17 23:24:43 -0400

    Another revision of the main function for the mixer application playing out a wav

--- source/AudioMixer/audio_mixer.cpp
+++ source/AudioMixer/audio_mixer.cpp
@@ -1,40 +1,89 @@
-
+#include <signal.h>
 #include <unistd.h>
 
 #include "BasicAudioChannel.h"
+#include "ClientHandler.h"
 #include "DebugMessage.h"
 #include "MultiChannelMixer.h"
 #include "PlaybackThread.h"
 
+#define AUDIO_FILE "/home/ct/data/dev/ece554_project/test/data/test_22050_1.wav"
+
+static bool app_signaled = false;
+
+void sig_handler(int num)
+{
+	TRACEF("Received signal [%d]\n", num);
+	app_signaled = true;
+}
+
+void setup_signals(void)
+{
+	for(int i = 0; i <= 15; i++)
+	{
+		signal(i, sig_handler);
+	}
+}
+
 int main(void)
 {
+	setup_signals();
+
 	PlaybackThread *playback = PlaybackThread::startPlaybackThread();
+	ClientHandler *client_handler = ClientHandler::startClientHandler(playback);
 
 	if(playback)
 	{
-		size_t i = 0;
+		BasicAudioChannel channel;
 
-		while(i++ < 5)
+		playback->addChannel(&channel);
+		FILE *pFile = fopen(AUDIO_FILE, "rb");
+
+		if(pFile)
 		{
-			BasicAudioChannel channel;
-			AudioSample_t sample = 0xBEEF;
+			fseek(pFile, 40, SEEK_SET);
 
-			for(size_t j = 0; j < 4096; j++)
+			int32_t data_size = 0;
+			fread(&data_size, sizeof(int32_t), 1, pFile);
+
+			AudioSample_t *samples = new AudioSample_t[data_size];
+
+			if(samples)
 			{
-				channel.push_back(sample);
+				fseek(pFile, 44, SEEK_SET);
+				fread(samples, data_size, 1, pFile);
 			}
 
-			playback->addChannel(&channel);
+			fclose(pFile);
+			pFile = NULL;
 
-			TRACEF("Main thread sleeping...[%zu]\n", i);
-			usleep(1000*1000*5);
+			size_t samples_to_push = 11264;
 
-			playback->removeChannel(&channel);
+			for(size_t i = 0; i < data_size/sizeof(AudioSample_t) && !app_signaled;)
+			{
+				size_t actual_samples_pushed = (i + samples_to_push < data_size/sizeof(AudioSample_t)) ? samples_to_push : data_size/sizeof(AudioSample_t)-i;
+				channel.push_back(&samples[i], actual_samples_pushed);
+				i+=samples_to_push;
+
+				usleep(1000*500);
+			}
+
+//			channel.push_back(samples, data_size/sizeof(AudioSample_t));
+			delete[] samples;
+			samples = NULL;
 		}
 
+		if(!app_signaled)
+			usleep(1000*1000*50);
 		TRACE("Shutting down playback thread...\n");
 		delete playback;
 		playback = NULL;
+	}
+
+	if(client_handler)
+	{
+		delete client_handler;
+		client_handler = NULL;
 	}
 
 	return 0;
