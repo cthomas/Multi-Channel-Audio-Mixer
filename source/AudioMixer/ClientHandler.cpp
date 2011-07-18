d3abe8e198739dc7fa032543b30394c4f2551c2b
Chris Thomas <chrtho@umd.umich.edu>
2011-07-18 14:31:19 -0400

    Stub in playing some wav's in ClientHandler using the ClientWorker's

--- source/AudioMixer/ClientHandler.cpp
+++ source/AudioMixer/ClientHandler.cpp
@@ -2,9 +2,13 @@
 #include <unistd.h>
 
 #include "AudioMixerInterface.h"
+#include "ClientWorker.h"
 #include "DebugMessage.h"
 
 #include "ClientHandler.h"
+
+#define PLAYBACK_FILE "test/data/test_22050_1.wav"
+#define VOICEOVER_FILE "test/data/voice_over.wav"
 
 ClientHandler::ClientHandler()
 	: BasicThread(1024*128)
@@ -14,6 +18,23 @@ ClientHandler::ClientHandler()
 
 ClientHandler::~ClientHandler()
 {
+	std::vector<ClientWorker*>::iterator itt = _workers.begin();
+
+	for(; itt != _workers.end(); ++itt)
+	{
+		if(*itt)
+		{
+			if(_mixer)
+			{
+				_mixer->removeChannel(*itt);
+			}
+
+			delete *itt;
+			*itt = NULL;
+		}
+	}
+
+	_workers.clear();
 	_mixer = NULL;
 }
 
@@ -42,8 +63,40 @@ void *ClientHandler::threadMain(void *data)
 	if(handler)
 	{
 		handler->setThreadIdentifier("ClientHandler");
+		size_t sleep_time_secs = 0;
 		while(!handler->shutdown())
 		{
+			if(sleep_time_secs == 5)
+			{
+				ClientWorker *worker = ClientWorker::startClientWorker();
+
+				if(worker)
+				{
+					if(handler->_mixer)
+					{
+						handler->_mixer->addChannel(worker);
+						worker->startPlaybackFile(PLAYBACK_FILE);
+					}
+
+					handler->_workers.push_back(worker);
+				}
+			}
+			else if(sleep_time_secs == 10)
+			{
+				ClientWorker *worker = ClientWorker::startClientWorker();
+
+				if(worker)
+				{
+					if(handler->_mixer)
+					{
+						handler->_mixer->addChannel(worker);
+						worker->startPlaybackFile(VOICEOVER_FILE);
+					}
+
+					handler->_workers.push_back(worker);
+				}
+			}
+			sleep_time_secs += 1;
 			usleep(1000*1000*1);
 		}
 	}
--- source/AudioMixer/ClientHandler.h
+++ source/AudioMixer/ClientHandler.h
@@ -1,9 +1,11 @@
 #ifndef _CLIENT_HANDLER_H_
 #define _CLIENT_HANDLER_H_
 
+#include <vector>
 #include "BasicThread.h"
 
 class AudioMixerInterface;
+class ClientWorker;
 
 class ClientHandler
 	: public BasicThread
@@ -13,6 +15,7 @@ private:
 	AudioMixerInterface *_mixer;
 
 	static void *threadMain(void *data);
+	std::vector<ClientWorker*> _workers;
 public:
 	~ClientHandler();
 	static ClientHandler *startClientHandler(AudioMixerInterface *mixer);
