ea04a309c83f795ad22dfd526340d9ce4a9ad4fe
Chris Thomas <chrtho@umd.umich.edu>
2011-07-18 14:30:04 -0400

    Cleanup of audio_mixer.cpp to just create and destroy the threads

--- source/AudioMixer/audio_mixer.cpp
+++ source/AudioMixer/audio_mixer.cpp
@@ -32,52 +32,16 @@ int main(void)
 	PlaybackThread *playback = PlaybackThread::startPlaybackThread();
 	ClientHandler *client_handler = ClientHandler::startClientHandler(playback);
 
+	while(1)
+	{
+		if(!app_signaled)
+			usleep(1000*1000*1);
+		else
+			break;
+	}
+
 	if(playback)
 	{
-		BasicAudioChannel channel;
-
-		playback->addChannel(&channel);
-		FILE *pFile = fopen(AUDIO_FILE, "rb");
-
-		if(pFile)
-		{
-			fseek(pFile, 40, SEEK_SET);
-
-			int32_t data_size = 0;
-			fread(&data_size, sizeof(int32_t), 1, pFile);
-
-			AudioSample_t *samples = new AudioSample_t[data_size];
-
-			if(samples)
-			{
-				fseek(pFile, 44, SEEK_SET);
-				fread(samples, data_size, 1, pFile);
-			}
-
-			fclose(pFile);
-			pFile = NULL;
-
-			size_t samples_to_push = 11264;
-
-			TRACE("Sleeping before we start streaming...\n");
-			usleep(1000*1000*10);
-			for(size_t i = 0; i < data_size/sizeof(AudioSample_t) && !app_signaled;)
-			{
-				size_t actual_samples_pushed = (i + samples_to_push < data_size/sizeof(AudioSample_t)) ? samples_to_push : data_size/sizeof(AudioSample_t)-i;
-				channel.push_back(&samples[i], actual_samples_pushed);
-				i+=samples_to_push;
-
-				usleep(1000*500);
-			}
-
-//			channel.push_back(samples, data_size/sizeof(AudioSample_t));
-			delete[] samples;
-			samples = NULL;
-		}
-
-		if(!app_signaled)
-			usleep(1000*1000*50);
-		TRACE("Shutting down playback thread...\n");
 		delete playback;
 		playback = NULL;
 	}
